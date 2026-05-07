2599185ec6661eb5e2e7cb6201b40881f9900126
Nitesh Giri <niteshgiri068@gmail.com>
2026-05-07 22:49:40 +0530

    Add new backend URL to CORS origins

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'https://mindcareai-nkg.vercel.app/',
-        'https://mind-care-ai-mauve.vercel.app/',
+        'https://mind-care-ai-sooty.vercel.app',
         'http://localhost:8008',
         'http://localhost:5173',
         process.env.FRONTEND_URL || 'http://localhost:3000'
